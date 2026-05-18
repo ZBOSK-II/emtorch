6abd11fbee6c5349105f79fadaa7d5604821424c
Konrad Grochowski <Konrad.Grochowski@pw.edu.pl>
2026-05-18 13:58:21 +0200

    files.py - opening in non-blocking mode

--- emtorch/subtasks/files.py
+++ emtorch/subtasks/files.py
@@ -7,6 +7,7 @@ Module holding sub-tasks related to files.
 """
 
 import logging
+import os
 from typing import Self
 
 from ..config import Config
@@ -40,19 +41,23 @@ class FileWriter(BasicSubTask):
         self._writer: None | StreamWriter = None
 
     def basic_start(self, context: CaseContext) -> bool:
+        lines = [line.evaluate(context) for line in self._lines]
+        data = "\n".join(lines).encode(self._encoding)
+
         path = self._path.evaluate(context)
         mode = ("a" if self._append else "w") + "b"
+        flags = os.O_WRONLY | os.O_CREAT | (os.O_APPEND if self._append else os.O_TRUNC)
         try:
-            # pylint: disable=consider-using-with
-            file = open(path, mode)
+            fd = os.open(path, flags, 0o666)
+            os.set_blocking(fd, False)
+            file = os.fdopen(fd, mode)
         except IOError as ex:
             self.logger.error(f"Open file failed '{path}' - {ex}")
             return False
 
-        lines = [line.evaluate(context) for line in self._lines]
-        data = "\n".join(lines).encode(self._encoding)
-
         self._writer = StreamWriter(self.name, file, data)
+        self._io.register(self._writer)
+
         return True
 
     def finish(self) -> BasicSubTask.Result:
